9945b641054614f70da05681ea1b73836b053381
Pete Savage <psavage@redhat.com>
2017-03-02 14:15:09 +0000

    Moved to new entrypoints compatibility

--- template_provider/__init__.py
+++ template_provider/__init__.py
@@ -5,13 +5,11 @@ class Management(object):
     pass
 
 
-@BaseProvider.add_base_type
 class TemplateProvider(BaseProvider):
     provider_types = {}
     category = "template"
 
 
-@TemplateProvider.add_provider_type
 class MyTemplateProvider(TemplateProvider):
     type_name = "myprovider"
     mgmt_class = Management
